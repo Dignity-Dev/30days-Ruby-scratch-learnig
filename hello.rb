32d7aefa7a7ab9f9b4f8326992b2a45bf2afc077
Basheer Haadi <bashirhaadiy@gmail.com>
2022-10-19 23:46:18 +0100

    update

--- hello.rb
+++ hello.rb
@@ -3,5 +3,9 @@ Puts "hello world" #it will print "hello world"
 # let work with input and output
 print "What is your Name?"
 name = gets
+love = "basket ball"
+age = 45
 print "My name is #{name}"
+print "I love playing  #{love}"
+print "I am #{age} years old"
 pirnt "New Name and Journey"
