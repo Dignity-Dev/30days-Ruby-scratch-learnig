38190e1c4be029423f711affe68157d7251eb05a
Basheer Haadi <bashirhaadiy@gmail.com>
2022-10-20 14:06:20 +0100

    love and age changed

--- hello.rb
+++ hello.rb
@@ -3,8 +3,8 @@ Puts "hello world" #it will print "hello world"
 # let work with input and output
 print "What is your Name?"
 name = gets
-love = "basket ball"
-age = 45
+love = "volley ball"
+age = 49
 print "My name is #{name}"
 print "I love playing  #{love}"
 print "I am #{age} years old"
